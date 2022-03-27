08bb581384992f7ca846b0e5b3282274fbd627b3
Uladzisalu <wladyslaw.tsemenko@gmail.com>
2022-03-27 18:17:53 +0300

    feat: first task complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello')
+const items = document.querySelectorAll('.item');
+
+items.forEach((item) => {
+  item.addEventListener('click', () => {
+    remove();
+    item.classList.add('active')
+  })
+});
+
+function remove() {
+  items.forEach((item) => {
+    item.classList.remove('active')
+  });
+
+}
