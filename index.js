64f967be07bda21112f79704fab8df6879630dab
Uladzisalu <wladyslaw.tsemenko@gmail.com>
2022-03-28 15:47:08 +0300

    feat: third task complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('hello')
+
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const wrap = document.querySelector('.wrapper');
+
+open.addEventListener('click', () => {
+  wrap.classList.add('show')
+})
+
+close.addEventListener('click', () => {
+  wrap.classList.remove('show')
+})
